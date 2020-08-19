82f9dc158cf36805ca745195e8b77d21e4763b54
Alix Wawa <alixwawa@yahoo.com>
2020-08-19 16:40:18 -0500

    hopefully this fixes it

--- models/user.js
+++ models/user.js
@@ -44,11 +44,11 @@ module.exports = function(sequelize, DataTypes) {
     }
   });
 
-  const oldShows = sequelize.define("oldShows", {
-    oldShows: {
-      type: DataTypes.INTEGER,
-      allowNull: false            
-  }
+  // const oldShows = sequelize.define("oldShows", {
+  //   oldShows: {
+  //     type: DataTypes.INTEGER,
+  //     allowNull: false            
+  // }
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
   User.prototype.validPassword = function(password) {
     return bcrypt.compareSync(password, this.password);
